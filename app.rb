9eebee0e683cd39057bb5dca07404ada03b548c1
Roy Kim <rykim79@gmail.com>
2018-08-17 23:56:44 -0400

    complete the website

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/reloader'
+also_reload 'lib/**/*.rb'
+require './lib/parcel'
+
+get '/' do
+  erb(:input)
+end
+
+get '/output' do
+  length = params.fetch('length').to_f
+  width = params.fetch('width').to_f
+  height = params.fetch('height').to_f
+  weight = params.fetch('weight').to_f
+  parcel = Parcel.new(length, width, height, weight)
+  @cost_to_ship = parcel.cost_to_ship()
+  erb(:output)
+end
